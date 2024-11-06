4d59ff13149d84b6818893fb967d60e4adb84f23
unknown <bartlomiej.wyszecki2@gmail.com>
2024-11-06 22:08:13 +0100

    start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'welcome',
+    loadComponent: () =>
+      import('./features/welcome/welcome.component').then(
+        (c) => c.WelcomeComponent
+      ),
+  },
+  {
+    path: 'employees',
+    loadComponent: () =>
+      import('./features/employees/employees.component').then(
+        (c) => c.EmployeesComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'welcome',
+    pathMatch: 'full',
+  },
+];
