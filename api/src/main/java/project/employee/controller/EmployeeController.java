248b6f11ad78a6c361e05b05962242f6af77ecf4
unknown <bartlomiej.wyszecki2@gmail.com>
2025-01-12 01:15:31 +0100

    postgreSQL database init

--- api/src/main/java/project/employee/controller/EmployeeController.java
+++ api/src/main/java/project/employee/controller/EmployeeController.java
@@ -1,14 +1,15 @@
 package project.employee.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
-    @RequestMapping(name = "/employee/{id}"`, method = RequestMethod.GET)
-    public String getEmployeeById(@PathVariable String id) {
-        return "Employee " + id;
+    @GetMapping("/{id}")
+    String getEmployee(@PathVariable String id) {
+        return "Employee" + " " + id;
     }
 }
---
+++ api/src/main/java/project/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package project.employee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.employee.Employee;
+
+@Repository
+public class EmployeeRepository {
+}
