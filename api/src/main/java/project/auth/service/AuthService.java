af80b39b59de0a70917a8c2fe95a9a37abcd061f
unknown <bartlomiej.wyszecki2@gmail.com>
2025-01-23 20:51:34 +0100

    security config

--- api/src/main/java/project/auth/service/AuthService.java
+++ api/src/main/java/project/auth/service/AuthService.java
@@ -1,5 +1,17 @@
 package project.auth.service;
 
-public class AuthService {
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import project.auth.dto.RegisterUserDTO;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final PasswordEncoder passwordEncoder;
+
+    public User registerUser(RegisterUserDTO)
+    {}
 }
