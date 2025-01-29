72488dac987e99410ebe2d38998d65228425bb73
unknown <bartlomiej.wyszecki2@gmail.com>
2025-01-29 23:13:21 +0100

    JwtUtil - validate token, get username from token

--- api/src/main/java/project/auth/config/JwtUtil.java
+++ api/src/main/java/project/auth/config/JwtUtil.java
@@ -1,7 +1,8 @@
 package project.auth.config;
 
-import java.security.Key;
 import java.util.Date;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,7 @@ import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
-    private final Key key;
+    private final SecretKey key;
     private final int expiration;
 
     public JwtUtil(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration}") int expiration) {
@@ -29,5 +30,27 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(key)
                 .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token);
+            
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
