7f9cddb9594b5944670f445121851568b78d63ec
unknown <bartlomiej.wyszecki2@gmail.com>
2025-02-16 21:52:35 +0100

    home test

---
+++ ui/features/home/Home.tsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-4xl font-bold">Welcome to Meme App</h1>
+      <p className="text-lg">
+        This is the home page of our application. Feel free to explore!
+      </p>
+    </div>
+  );
+}; 
