66774f7478076a99a0f37a63764cbfd1a397bfc8
unknown <bartlomiej.wyszecki2@gmail.com>
2025-02-27 23:26:23 +0100

    local cors config added

--- api/src/main/java/project/auth/config/LocalCorsConfig.java
+++ api/src/main/java/project/auth/config/LocalCorsConfig.java
@@ -17,7 +17,7 @@ public class LocalCorsConfig {
             public void addCorsMappings(@NonNull CorsRegistry registry)
             {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:300")
+                    .allowedOrigins("http://localhost:3000")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
