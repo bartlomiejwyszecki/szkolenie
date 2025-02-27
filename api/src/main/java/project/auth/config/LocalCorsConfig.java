4edecccd5cba0e02b0eafa92b3ffaa281a7e3f60
unknown <bartlomiej.wyszecki2@gmail.com>
2025-02-27 23:20:58 +0100

    cors config

---
+++ api/src/main/java/project/auth/config/LocalCorsConfig.java
@@ -0,0 +1,27 @@
+package project.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("local")
+public class LocalCorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry)
+            {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:300")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
