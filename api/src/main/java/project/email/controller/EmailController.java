c3c2a5214da1d3cc86c6d67a4deadc92ba037967
unknown <bartlomiej.wyszecki2@gmail.com>
2025-04-07 22:37:00 +0200

    email controller

---
+++ api/src/main/java/project/email/controller/EmailController.java
@@ -0,0 +1,32 @@
+package project.email.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import project.email.EmailService;
+import project.employee.dto.CreateEmployeeDTO;
+import project.employee.model.Employee;
+import project.employee.service.EmployeeService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/email")
+@Tag(name = "Auth")
+public class EmailController {
+    private final EmailService emailService;
+
+    @Autowired
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> sendEmail(@RequestBody SendEmailDTO email) {
+    }
+}
