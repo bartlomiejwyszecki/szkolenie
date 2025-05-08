9da22a1fe698d21cab4f539a5557fb8b576fca61
unknown <bartlomiej.wyszecki2@gmail.com>
2025-05-08 21:30:35 +0200

    User entity refactor

--- api/src/main/java/project/user/model/User.java
+++ api/src/main/java/project/user/model/User.java
@@ -58,14 +58,4 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column()
     private Sex sex;
-    
-    @Column(nullable = false)
-    @Builder.Default
-    private boolean verified = false;
-
-    @Column(unique = true)
-    private String verificationCode; 
-
-    @Column
-    private Instant verificationExpiry;
 }
